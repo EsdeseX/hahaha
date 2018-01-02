c4f7ae04efcbaae7f58d4265dee8a63d8420c56e
EsdeseX <32089980+EsdeseX@users.noreply.github.com>
2018-01-02 09:55:09 +0800

    比大小

---
+++ 比大小.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	scanf("%x",&a);
+	scanf("%x",&b);
+	if(a>b)
+	{
+		printf("1");
+	}
+	else if(a==b)
+	{
+		printf("0");
+	}
+	else{
+		printf("-1");
+	}
+	return 0;
+	}
